fb4b5d4f2ce3f7abf1d97f41d7461ab05c00b1d0
caoyujie6 <1106377039@qq.com>
2018-12-17 14:55:27 +0800

    222gfas

---
+++ src/test/java/com/example/demo22222/Demo22222ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo22222;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo22222ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
